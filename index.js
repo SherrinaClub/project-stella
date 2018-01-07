98f6c6ea79e139449f96e2e78b1e68a7b1fd07b0
MC Qu <qumingcheng1020@hotmail.com>
2018-01-06 21:56:50 -0700

    Fix heroku-6

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cookieSession({
-    maxAga: 30 * 24 * 60 * 60 * 1000,
+    maxAga: 40 * 24 * 60 * 60 * 1000,
     keys: [keys.cookieKey]
   })
 );
@@ -20,6 +20,19 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production file
+  // like main.js and main.css file
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT);
